c0ef978600fe62fa01ddd78b067d6f6a864ea08d
amoora <omardiab.m@gmail.com>
2025-01-20 22:21:45 -0500

    spiderman

---
+++ Lab2A.py
@@ -0,0 +1,5 @@
+name1 = input("Enter a name: ")
+name2 = input("Enter another name: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+print("Once upon a time, there was a person named " + name1 + " who had a child named " + name2 + ". This child would " + verb + " " + adverb + " while singing to strangers.")
---
+++ Lab2C.py
@@ -0,0 +1,8 @@
+pjw = int(input("Enter a width: "))
+h = int(input("Enter a height: "))
+
+p = 2*(h+w)
+a = (h*w)
+
+print("The area is " + str(a))
+print("The perimeter is " + str(p))
